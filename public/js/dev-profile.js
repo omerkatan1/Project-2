8e602f7944cbe507608d8013b96fbfb290e44776
CJ Conrad <carlj.conrad@CJs-MacBook-Pro.local>
2020-01-09 16:14:04 -0800

    lilys merge

--- public/js/dev-profile.js
+++ public/js/dev-profile.js
@@ -1,37 +1,39 @@
-$(document).ready(function () {
-// get user info object
-//  1. build empty object for values needed to populate user info box
-function getUserInfo() {
-    var bigData = {
-        developer_name: "",
-        developer_email: "",
-        developer_staus: "",
-        developer_intro: "",
-        developer_techniques: [],
-        completeProjects: [],
-    };
-    //  2. get user id. api call to /spi/org-data
-    $.get("/api/org_data").then(function (data) {
-        bigData.developer_name= data.first_name + ' ' + data.last_name;
-        bigData.developer_email= data.email;
-        bigData.developer_staus= data.status;
-        bigData.developer_intro= data.intro;
-        var techString = data.techniques;
-        var techArray = techString.split(";");
-        bigData.developer_techniques = techArray;
-        console.log(bigData.developer_techniques);
-        loadCompleteProjects();
-    });
-    function loadCompleteProjects() {
-        $.get("/api/project").then(function (data) {
-            console.log(data);
-            for (var i = 0; i < data.length; i++) {
-                if (data[i].status === "Finished") {
-                    bigData.completeProjects.push(data[i]);
+// $(document).ready(function () {
+    // get user info object
+    //  1. build empty object for values needed to populate user info box
+    $("#personalProfile").on("click", function(event) {
+        event.preventDefault();
+        console.log("Something");
+        var bigData = {
+            developer_name: "",
+            developer_email: "",
+            developer_staus: "",
+            developer_intro: "",
+            developer_techniques: [],
+            completeProjects: [],
+        };
+        //  2. get user id. api call to /spi/org-data
+        $.get("/api/org_data").then(function (data) {
+            bigData.developer_name = data.first_name + ' ' + data.last_name;
+            bigData.developer_email = data.email;
+            bigData.developer_staus = data.status;
+            bigData.developer_intro = data.intro;
+            var techString = data.techniques;
+            var techArray = techString.split(";");
+            bigData.developer_techniques = techArray;
+            console.log(bigData.developer_techniques);
+            loadCompleteProjects();
+        });
+        function loadCompleteProjects() {
+            $.get("/api/project").then(function (data) {
+                console.log(data);
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].status === "Finished") {
+                        bigData.completeProjects.push(data[i]);
+                    }
                 }
-            }
-            console.log(bigData);
-            var source = `<div class="row justify-content-center">
+                console.log(bigData);
+                var source = `<div class="row justify-content-center">
                             <div class="col-sm-4 box" id="user-image">
                                 <img src="img/sqwashAvater.jpg" alt="profile-image" id="avatar">
                             </div>
@@ -64,15 +66,10 @@ function getUserInfo() {
                             </div>
                         </div>`;
 
-            var template = Handlebars.compile(source);
-            $("#profile-main").html(template(bigData));
-        })
-    }
-};
-getUserInfo();
-//  4. pass object to template literal (html model) defined as var source
-//  5. handlebars.compile(source) defined as var template
-//  6. element.html(template(pass data object here))
-// get completed project list
-// 
-});
+                var template = Handlebars.compile(source);
+                $("#profile-main").html(template(bigData));
+            })
+        }
+    });
+
+// });
